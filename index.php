96dbd2de5ce798cdefda4e684bc56af80eea0225
Tim McHugh <tim@digitaltim.com>
2017-04-21 14:21:24 -0400

    Add Twig template engine
    Use php -S localhost:8000 to view in browser
    https://www.codecourse.com/lessons/learn-slim-3/1439

--- index.php
+++ index.php
@@ -2,21 +2,37 @@
 
 require 'vendor/autoload.php';
 
+// Create app
 $app = new \Slim\App([
 	'settings' => [
 		'displayErrorDetails' => true,
 	]
 ]);
 
+// Get container
 $container = $app->getContainer();
 
+// Register component on container
 $container['greeting'] = function () {
 	echo 'Home';
 	return 'Hello from the container';
 };
 
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/resources/views', [
+        'cache' => false
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
 $app->get('/', function () {
-	echo $this->none;
+	echo $this->greeting;
 });
 
 $app->run();
